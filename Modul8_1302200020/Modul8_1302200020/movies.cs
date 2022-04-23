db3246715c66b6a2b1a382da10da5c114b2fb120
rizal <100459809+Heiasan@users.noreply.github.com>
2022-04-23 17:42:55 +0700

    API

---
+++ Modul8_1302200020/Modul8_1302200020/movies.cs
@@ -0,0 +1,15 @@
+﻿namespace Modul8_1302200020
+{
+    public class Movies
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string[] Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movies()
+        {
+
+        }
+    }
+}
